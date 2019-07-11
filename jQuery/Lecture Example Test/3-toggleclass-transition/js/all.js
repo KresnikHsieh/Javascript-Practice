86605bd85bb2234902b32131e75afc156303aeab
cd <x19921111@gmail.com>
2019-07-11 22:26:04 +0800

    add jQuery Practice toggleClass

---
+++ jQuery/Lecture Example Test/3-toggleclass-transition/js/all.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//   $('.button').click(function(event) {
+//     $('.text').toggleClass('active');
+//     // $('.text').addClass('active');
+//     // $('.text').removeClass('active');
+//   });
+// });
+
+
+
+$(document).ready(function(){
+	$('.button').click(function(e){
+		$('.text').toggleClass('active');
+
+	});
+});
